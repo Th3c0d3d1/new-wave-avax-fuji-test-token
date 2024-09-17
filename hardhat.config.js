607462c393c9505d033c9f39b937aac7c7b9c6f3
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-09-17 16:00:29 -0400

    adj config for avax deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,9 +6,13 @@ const privateKeys = process.env.PRIVATE_KEYS || '';
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    hardhat: {},
-    avalancheFuji: {
-      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+    avalanchefuji: {
+      url: `https://avalanche-fuji.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      chainId: 43113,
+      accounts: privateKeys.split(',')
+    },
+    avalanchemainnet: {
+      url: `https://avalanche-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       chainId: 43113,
       accounts: privateKeys.split(',')
     }
